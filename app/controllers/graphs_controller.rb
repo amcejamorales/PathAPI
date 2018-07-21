4b02a7d05209799ed29fa0a026e95fb95c5d0756
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-07-21 16:20:42 -0700

    Test and write 'index' action for graphs controller.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,12 +1,14 @@
 class GraphsController < ApplicationController
 
   def index
+    graphs = Graph.all
+    render json: graphs.as_json(except: [:created_at, :updated_at], status: :ok)
   end
 
   def show
   end
 
   def create
-  end 
+  end
 
 end
--- test/controllers/graphs_controller_test.rb
+++ test/controllers/graphs_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 describe GraphsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "index" do
+    it "returns a json array" do
+      get graphs_url
+      must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+  end # index
+
 end
