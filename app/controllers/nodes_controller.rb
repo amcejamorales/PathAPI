0a09ac7500a781dfebe71715d2ac7c5afe1f1b2a
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-07-21 17:04:16 -0700

    Test and write index action for nodes controller.

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,12 +1,17 @@
 class NodesController < ApplicationController
 
   def index
+    nodes = Node.all
+    render json: nodes.as_json(
+      except: [:created_at, :updated_at],
+      status: :ok
+    )
   end
 
   def show
   end
 
   def create
-  end 
+  end
 
 end
--- test/controllers/nodes_controller_test.rb
+++ test/controllers/nodes_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 describe NodesController do
   describe "index" do
-    it "returns a json array" do 
+    it "returns a json array" do
+      get nodes_url
+      must_respond_with :success
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
     end
   end # index
 
